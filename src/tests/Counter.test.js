6ee8452f1c204b048ce6e4e2a7b46109d7b76feb
pooky-programs <smyers2@ufl.edu>
2024-04-04 13:09:35 -0500

    added tests and screenshot

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -3,20 +3,27 @@
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter/>);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByRole('heading')).toHaveTextContent('Counter');
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByRole('paragraph')).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByRole('button').getByText('+'));
+  expect(screen.getByRole('paragraph')).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByRole('button').getByText('-'));
+  expect(screen.getByRole('paragraph')).toHaveTextContent('-1');
 });
